2ba38db7b0ac2801f649c806a29ab1cc7872470e
CzarFontanella <cesar.fontanella@gmail.com>
2025-10-08 21:19:19 -0300

    feat: Implementada a pagina de Login

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,15 @@
+import "./Login.css"
+
+export default function Login() {
+  return (
+    <div className="login-container">
+        <form id="login-card" action="">
+            <h2>Login</h2>
+            <input type="text" placeholder='Usuário' />
+            <input type="password" placeholder='Senha...' />
+            <button type="submit">Login</button>
+        </form>
+    </div>
+  )
+}
+
